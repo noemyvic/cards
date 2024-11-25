b9ead4523e6a1d36ec165080afea69e37af85cea
noemyvic <00001099743291sp@al.educacao.sp.gov.br>
2024-11-25 13:36:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'fisica',
+    'qual a formula utilizada para calcular velocidade?',
+    'V= T/S '
+)
+
+criaCartao(
+    'Geografia',
+    'Onde está localizado o japão?',
+    'leste da Ásia'
+)
+
+criaCartao(
+    'Historia ',
+    'O que foi a peste negra?',
+    'Uma pandemia que ocorreu na Europa entre 1347 e 1353, causada pela bactéria Yersinia pestis'
+)
+
+criaCartao(
+    'ingles',
+    'Menina em Inglês?',
+    'Girl'
+)
